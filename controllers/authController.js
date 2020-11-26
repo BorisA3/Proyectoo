979705744ef651863f72919b2944923e40799763
extrabrashpit <lalo_brash@hotmail.com>
2020-11-26 16:16:26 -0600

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,27 +3,32 @@ const passport = require("passport");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 const Usuario = mongoose.model("Usuarios");
-const enviarCorreo = require("../handlebars/email");
+const enviarCorreo = require("../handlers/email");
 const { send } = require("process");
 
 // Se encarga de autenticar el usuario y de redireccionarlo
 exports.autenticarUsuario = passport.authenticate("local", {
-  successRedirect: "/administrar",
-  failureRedirect: "/iniciar-sesion",
+  successRedirect: "/",
+  failureRedirect: "/",
   failureFlash: true,
   badRequestMessage: ["Debes ingresar tus credenciales"],
 });
 
 // Cerrar la sesión del usuario
 exports.cerrarSesion = (req, res, next) => {
+  const messages = [];
+
   // Cierra la sesión
   req.logout();
 
-  req.flash("success", [
-    "Has cerrado correctamente tu sesión. ¡Vuelve pronto!",
-  ]);
+  messages.push({
+    message: "Has cerrado correctamente tu sesión. ¡Vuelve pronto!",
+    alertType: "success",
+  });
 
-  return res.redirect("/iniciar-sesion");
+  req.flash("messages", messages);
+
+  return res.redirect("/home");
 };
 
 // Mostrar el formulario de restablecer la contraseña
