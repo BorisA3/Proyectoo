a6683b766183d821a4a4cf8273669818dcf22c34
extrabrashpit <lalo_brash@hotmail.com>
2020-11-26 13:30:07 -0600

    renombre de ruta

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,13 +3,13 @@ const passport = require("passport");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 const Usuario = mongoose.model("Usuarios");
-const enviarCorreo = require("../handlers/email");
+const enviarCorreo = require("../handlebars/email");
 const { send } = require("process");
 
 // Se encarga de autenticar el usuario y de redireccionarlo
 exports.autenticarUsuario = passport.authenticate("local", {
   successRedirect: "/administrar",
-  failureRedirect: "/",
+  failureRedirect: "/iniciar-sesion",
   failureFlash: true,
   badRequestMessage: ["Debes ingresar tus credenciales"],
 });
